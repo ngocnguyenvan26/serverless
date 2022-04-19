63fcfd2f50fa00a376f8e57633f32a3cc3c30ef0
ngocnguyen <ngoc.nguyenvan@powergatesoftware.com>
2022-04-19 05:13:03 +0000

    add config.

--- app/config/index.ts
+++ app/config/index.ts
@@ -4,3 +4,7 @@ const dotenvPath = path.join(__dirname, '../', `config/.env.${process.env.NODE_E
 dotenv.config({
   path: dotenvPath,
 });
+export default {
+  port: process.env.APP_PORT,
+  portSchedule: process.env.APP_PORT_SCHEDULE || '2021',
+};
