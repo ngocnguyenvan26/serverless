f7111952550ab7b27475f5774db985278fc2213c
ngocnguyen <ngoc.nguyenvan@powergatesoftware.com>
2022-04-20 15:40:26 +0000

    test 22:40

--- app/modules/products/productService.ts
+++ app/modules/products/productService.ts
@@ -47,6 +47,7 @@ export default class ProductService {
       let b = 1;
       let c = 1;
       let d = 2;
+      let e = 2;
       const exec = await qB.limit(limit).exec();
       console.log('last key', exec.lastKey)
       console.log('json', exec.toJSON())
